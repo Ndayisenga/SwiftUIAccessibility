de3323a51905ad8919973e4ae016a75c26321ebd
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-04 11:48:33 +0200

    SwiftUI Access

--- SwiftUIAccessibility/ContentView.swift
+++ SwiftUIAccessibility/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedNumber: Int = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("1")
+                        .padding()
+                    Text("2")
+                        .padding()
+                    Text("3")
+                        .padding()
+                    Text("4")
+                        .padding()
+                    Text("5")
+                        .padding()
+                    Text("6")
+                        .padding()
+                }
+                .accessibilityElement()
+                .accessibilityLabel(Text("Numbers Row"))
+                .accessibilityValue(Text(String(self.selectedNumber)))
+                .accessibilityAdjustableAction { direction in
+                    switch direction {
+                    case .increment:
+                        self.selectedNumber += 1
+                    case .decrement:
+                        guard self.selectedNumber > 1 else {
+                            return
+                        }
+                        self.selectedNumber -= 1
+                    @unknown default:
+                        break
+                    }
+                }
+                .navigationTitle("SwiftUI Accessibility")
+            }
+        }
     }
 }
 
