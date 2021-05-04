5c272da11d576e519729795802cb444964c93404
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-04 10:59:02 +0200

    Initial Commit

---
+++ SwiftUIAccessibility/SwiftUIAccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAccessibilityApp.swift
+//  SwiftUIAccessibility
+//
+//  Created by Ndayisenga Jean Claude on 04/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
